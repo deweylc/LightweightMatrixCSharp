d5cda900380b3a7d346c8bd765318e6b70dcf37d
Dewey <380350751@qq.com>
2019-10-22 15:44:29 +0800

    添加了实例
    添加了一些实例用以进行测试与示意

---
+++ Example/Matrix/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LightweightMatrixCSharp;
+namespace _Matrix_
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Matrix m = new Matrix();
+            m=Matrix.Parse("1 0 0\r\n" +
+                "1 1 0\r\n" + 
+                "1 1 5\r\n");
+            Console.WriteLine(m.ToString());
+            Console.WriteLine(m.Det());
+            Console.Read();
+        }
+    }
+}
